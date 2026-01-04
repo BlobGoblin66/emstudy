e8a484764eac766498d6c09f7b29677846768b12
BlobGoblin66 <ellen.apswoude@gmail.com>
2026-01-04 11:59:58 +1100

    added code

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-placeholder
+// -------------------------
+// 1. IMAGES LIST
+// -------------------------
+const images = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg",
+    "images/pic4.jpg"
+];
+
+// -------------------------
+// 2. SLIDESHOW SETTINGS
+// -------------------------
+let current = 0;
+const interval = 5000; // 5 seconds per slide
+
+const slideshow = document.getElementById("slideshow");
+
+// Preload images
+images.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
+
+// Set first image
+slideshow.style.backgroundImage = `url('${images[0]}')`;
+
+// Slideshow loop with film grain dissolve
+setInterval(() => {
+    current = (current + 1) % images.length;
+
+    // fade out
+    slideshow.style.opacity = 0;
+    slideshow.style.filter = "blur(2px) brightness(1.1)";
+
+    setTimeout(() => {
+        // switch image
+        slideshow.style.backgroundImage = `url('${images[current]}')`;
+
+        // fade in
+        slideshow.style.opacity = 1;
+        slideshow.style.filter = "blur(0px) brightness(1.0)";
+    }, 1800); // match CSS transition
+}, interval);
